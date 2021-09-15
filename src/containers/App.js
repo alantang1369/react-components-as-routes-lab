6c65410e765256376d8ffa7ae821bfc49dcf706d
alantang1369 <giabao_tang@yahoo.com>
2021-09-14 17:50:31 -0700

    commit tree 49b344b4add2071835cd7864c27c0e2aef4d1f24

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,6 +14,15 @@ const App = (props) => {
   return (
     <Router>
       {/*{code here}*/}
+      <div>
+        <NavBar/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/movies" component={Movies}/>
+        <Route path="/directors" component={Directors}/>
+        <Route path="/actors" component={Actors}/>
+        
+       
+      </div>
     </Router>
   );
 };
